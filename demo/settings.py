ba72ae0756932207b41ac155106c41334b0de588
Omkar Mogaveera <omkarmogaveera@gmail.com>
2026-05-25 20:35:35 +0530

    switching to a simpler storage

--- demo/settings.py
+++ demo/settings.py
@@ -71,7 +71,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
